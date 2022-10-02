7fee1ceeee0443fc5df172e40b1f2dca6e37994c
arijitnus <66009368+arijitnus@users.noreply.github.com>
2022-10-02 17:24:44 +0800

    Update Supplementary Figure 1B, 1D and 2.R

--- Supplementary figures/Supplementary Figure 1B, 1D and 2.R
+++ Supplementary figures/Supplementary Figure 1B, 1D and 2.R
@@ -50,21 +50,32 @@ ggsave(
 )
 dev.off()
 
+#Fig. S1B for genome size, completeness and contamination across datasets
+library(readxl)
+library(dplyr)
+library(ggplot2)
+all_soil_mags<-read_excel("/Users/arijitmukherjee/Documents/Phytobiome/BGC_isolates/New_final_data/MAGs/Earth_MAGs/earthmicrobiome_bgc_data (1).xlsx",col_names = T,skip = 0,sheet = "soil_bgc")
+all_plant_mags<-read_excel("/Users/arijitmukherjee/Documents/Phytobiome/BGC_isolates/New_final_data/MAGs/Earth_MAGs/earthmicrobiome_bgc_data (1).xlsx",col_names = T,skip = 0,sheet = "majorplant")
+dim(all_soil_mags)
+dim(all_plant_mags)
+plant_isolates<-read_excel("/Users/arijitmukherjee/Documents/Phytobiome/BGC_isolates/New_final_data/Network_Annotations_Full_all_isolates.xlsx",sheet = "Filtered",col_names = T,skip = 0)
+soil_isolates<-read_excel("/Users/arijitmukherjee/Downloads/genome_quality_info_all_soil_isolates.xlsx",sheet = "Sheet1",col_names = T,skip = 0)
 
 
-#======================#=============
-#Fig. S1B
-#Range for completeness in horizontal box plot; plot for completeness among three datasets
-boxplot(all_soil_mags$completeness)
-boxplot(all_plant_mags$Completeness)
-completeness_df<-data.frame(Datasets=c(rep("Plant_isolates",2761),rep("Plant_MAGs",1453),rep("Soil_MAGs",1300)),
-                            completeness=c(plant_isolates$Completeness,all_plant_mags$Completeness,all_soil_mags$completeness))
+df$Data<-factor(df$Data,levels=c("Plant isolates","Soil isolates","Plant MAGs","Soil MAGs"))
+
+completeness_df<-data.frame(Datasets=c(rep("Plant isolates",2761),rep("Plant MAGs",1453),rep("Soil MAGs",1300),rep("Soil isolates",2557)),
+                            completeness=c(plant_isolates$Completeness,all_plant_mags$Completeness,all_soil_mags$completeness,soil_isolates$completeness))
+completeness_df$Datasets<-factor(completeness_df$Datasets,levels = c("Plant isolates","Soil isolates","Plant MAGs","Soil MAGs"))
+
+
 box_complete<-ggplot(completeness_df,aes(x=Datasets,y=completeness,fill=Datasets))+
   geom_boxplot(width=0.2)+
   coord_flip()+
   theme_classic()+
   ylab("Completeness (%)")+
   xlab("")+
+  #xlim(c(25,100))+
   theme(axis.text.y = element_blank())+
   theme(axis.text.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
@@ -79,16 +90,15 @@ ggsave(
   width = 6,
   height = 3,
   units = "in",
-  dpi = 700,
+  dpi = 400,
 )
 dev.off()
 
 
-#Fig. S1B=========##+==========
-#Plotting contamination among all three datasets
-
-contam_df<-data.frame(Datasets=c(rep("Plant_isolates",2761),rep("Plant_MAGs",1453),rep("Soil_MAGs",1300)),
-                      contam=c(plant_isolates$Contamination,all_plant_mags$Contamination,all_soil_mags$contamination))
+##Plot the contamination distributions for genomes across datasets
+plant_isolates$Contamination
+contam_df<-data.frame(Datasets=c(rep("Plant_isolates",2761),rep("Plant_MAGs",1453),rep("Soil_MAGs",1300),rep("Soil isolates",2549)),
+                      contam=c(plant_isolates$Contamination,all_plant_mags$Contamination,all_soil_mags$contamination,soil_isolates$contamination))
 
 box_contamination<-ggplot(contam_df,aes(x=Datasets,y=contam,fill=Datasets))+
   geom_boxplot(width=0.2)+
@@ -114,10 +124,15 @@ ggsave(
 )
 dev.off()
 
-#Fig. S1B#==================================
-#Genome size distribution among three datasets
-GS_df<-data.frame(Datasets=c(rep("Plant_isolates",2761),rep("Plant_MAGs",1453),rep("Soil_MAGs",1300)),
-                  Genome_size=c(plant_isolates$Genome_Size/10^6,all_plant_mags$`Genome_size (bp)`/10^6,all_soil_mags$genome_length/10^6))# Genome size has been converted into Mbp
+
+####GS comparisons
+
+GS_df<-data.frame(Datasets=c(rep("Plant isolates",2761),rep("Plant MAGs",1453),rep("Soil MAGs",1300),rep("Soil isolates",2557)),
+                  Genome_size=c(plant_isolates$Genome_Size/10^6,all_plant_mags$`Genome_size (bp)`/10^6,all_soil_mags$genome_length/10^6,soil_isolates$length/10^6))# Genome size has been converted into Mbp
+
+GS_df$Datasets<-factor(GS_df$Datasets,levels = c("Plant isolates","Soil isolates","Plant MAGs","Soil MAGs"))
+
+
 
 box_GS<-ggplot(GS_df,aes(x=Datasets,y=Genome_size,fill=Datasets))+
   geom_boxplot(width=0.2)+
@@ -142,6 +157,8 @@ ggsave(
   dpi = 700,
 )
 dev.off()
+
+
 
 #Fig.S1D===========================
 #Stack bar plot for three datasets
